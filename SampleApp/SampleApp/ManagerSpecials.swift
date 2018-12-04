e08751d8aafe389a6c858759f354c53252cc7de1
nirmala <nirmala.jraman@gmail.com>
2018-12-04 13:59:36 -0800

    Initial Commit 12/4
    - Added ManagerSpecials model

---
+++ SampleApp/SampleApp/ManagerSpecials.swift
@@ -0,0 +1,12 @@
+//
+//  ManagerSpecials.swift
+//  SampleApp
+//
+//  Created by Nirmala Venkat on 12/4/18.
+//  Copyright © 2018 nirmala. All rights reserved.
+//
+
+import Foundation
+
+
+
---
+++ SampleApp/SampleApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Nirmala Venkat on 12/4/18.
+//  Copyright © 2018 nirmala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
